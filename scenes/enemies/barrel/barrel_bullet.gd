0c9a1945e6902500402f6ef71fb755ed4d597aa2
DPhngNam <pnam3072004@gmail.com>
2025-10-25 22:47:38 +0700

    fix: wrong stage's name

--- scenes/enemies/barrel/barrel_bullet.gd
+++ scenes/enemies/barrel/barrel_bullet.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 	gravity_scale = 0
 	linear_velocity = Vector2(speed, 0).rotated(rotation)
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	if global_position.distance_to(start_position) > max_distance:
 		queue_free()
 
@@ -18,9 +18,10 @@ func _on_hit_area_2d_hitted(_area: Variant) -> void:
 	queue_free() 
 func _on_body_entered(_body: Node) -> void: 
 	queue_free() 
-func _on_hit_area_2d_area_entered(area: Area2D) -> void:
+func _on_hit_area_2d_area_entered(_area: Area2D) -> void:
 	queue_free()  
-func _on_hit_area_2d_body_entered(body: Node2D) -> void:
+func _on_hit_area_2d_body_entered(_body: Node2D) -> void:
 	queue_free()  
-func _on_hit_area_2d_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+func _on_hit_area_2d_area_shape_entered(_area_rid: RID, _area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
 	queue_free()
+	
