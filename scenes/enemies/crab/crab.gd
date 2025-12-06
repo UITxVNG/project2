59c325f5af707689b10be2f4f2f57e1e51ceae7c
Hà Bảo Trân <144417841+hbtne@users.noreply.github.com>
2025-12-06 12:35:24 +0700

    Fix/ hurt state crab (#41)
    * Update saw_blade.png.import
    
    * fix/hurt-state-crab

--- scenes/enemies/crab/crab.gd
+++ scenes/enemies/crab/crab.gd
@@ -3,3 +3,24 @@ extends EnemyCharacter
 func _ready() -> void:
 	fsm = FSM.new(self, $States, $States/Run)
 	super._ready()
+
+
+func take_damage(a, b = null) -> void:
+	var hit_dir: Vector2
+	var damage: float
+	if typeof(a) == TYPE_VECTOR2 and b != null:
+		hit_dir = a
+		damage = float(b)
+	else:
+		hit_dir = Vector2.ZERO
+		damage = float(a)
+
+	health -= damage
+
+	if health <= 0:
+		_die()
+
+
+
+func _die() -> void:
+	queue_free()
